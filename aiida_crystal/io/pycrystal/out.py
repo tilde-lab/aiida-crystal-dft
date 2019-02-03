a79007b6bd40bf8f6eb3eb3cc531c399013f4ac6
Andrey Sobolev <andrey.n.sobolev@gmail.com>
2019-02-04 00:39:47 +0500

    Pycrystal-based out file parser (WIP)

--- aiida_crystal/io/pycrystal/out.py
+++ aiida_crystal/io/pycrystal/out.py
@@ -11,8 +11,24 @@ def parse(out_file):
     :param out_file: A name of file to parse
     :return: Parsed data
     """
+    out_params = {}
     if not CRYSTOUT.acceptable(out_file):
         raise FileNotFoundError("{} is not a valid CRYSTAL output file".format(out_file))
     result = CRYSTOUT(out_file)
-
-    return result.info
+    info = result.info
+    # Parameters from pycrystal parsing result as given in pwscf output-parameters
+    out_params['creator_name'] = "CRYSTAL"
+    out_params['creator_version'] = info['prog']
+    out_params['exchange_correlation'] = info['H']   # dft prefix omitted as there can be HF calculations
+    out_params['energy'] = info['energy']
+    out_params['energy_units'] = 'eV'  # pycrystal converts Ha to eV
+    out_params['energy_accuracy'] = abs(info['e_accuracy'])
+    out_params['energy_accuracy_units'] = 'eV'  # this also is converted
+    out_params['monkhorst_pack_grid'] = [int(i) for i in info['k'].split('x')]
+    out_params['number_of_atoms'] = info['n_atoms']
+    out_params['number_of_electrons'] = info['n_electrons']
+    out_params['number_of_symmetries'] = info['n_symops']
+    out_params['parser_info'] = 'pycrystal 1.0.0.2'
+    out_params['parser_warnings'] = info['warns']
+    out_params['scf_iterations'] = info['ncycles']   # a list of scf iteration numbers at each ionic step
+    return out_params
--- aiida_crystal/io/tests/test_out_pycrystal.py
+++ aiida_crystal/io/tests/test_out_pycrystal.py
@@ -3,7 +3,7 @@ A test of pycrystal-based output parser
 """
 
 import os
-from pprint import pprint
+# from pprint import pprint
 from aiida_crystal.io.pycrystal.out import parse
 from aiida_crystal.tests import TEST_DIR
 
@@ -12,5 +12,6 @@ def test_out():
     """A test of parsing out file"""
     out_file = os.path.join(TEST_DIR, 'output_files', 'mgo_sto3g_opt.crystal.out')
     res = parse(out_file)
-    pprint(res)
-    assert res
+    # pprint(res)
+    assert res['creator_name'] == 'CRYSTAL'
+    assert res['creator_version'] == '17 1.0.1'
