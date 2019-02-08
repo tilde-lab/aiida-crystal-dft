cca9deabb8cf84a917386cc94cb031bcc4bccb77
Andrey Sobolev <andrey.n.sobolev@gmail.com>
2019-02-09 00:16:59 +0500

    Turned off calculation tests

--- aiida_crystal/calculations/tests/test_crystal_calc.py
+++ aiida_crystal/calculations/tests/test_crystal_calc.py
@@ -1,8 +1,14 @@
 #  Copyright (c)  Andrey Sobolev, 2019. Distributed under MIT license, see LICENSE file.
 
+# noinspection PyUnresolvedReferences
 from aiida_crystal.tests.fixtures import *
+
 
 def test_store_calc(crystal_calc):
     calc = crystal_calc
     calc.store_all()
     assert calc.pk is not None
+    assert calc.inp.parameters.pk is not None
+    assert calc.inp.structure.pk is not None
+    assert calc.inp.settings.pk is not None
+
