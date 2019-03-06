Several bugs fixed in properties calculation

aiida_crystal/parsers/properties.py
@@ -60,7 +60,7 @@ class PropertiesParser(Parser):
             return success, node_list
 
         # Check the folder content is as expected
-        list_of_files = out_folder.get_content_list()
+        list_of_files = out_folder.get_folder_list()
         output_files = self._calc.retrieve_list
         # Note: set(A) <= set(B) checks whether A is a subset
         if set(output_files) <= set(list_of_files):
@@ -119,7 +119,6 @@ class PropertiesParser(Parser):
             raise ValueError("Sorry, didn't find dos info in fort.25")
 
         from aiida.orm import DataFactory
-
         array_data = DataFactory("array")()
         array_data.set_array("dos", np.vstack((data["e"], data["dos"])))
         return array_data