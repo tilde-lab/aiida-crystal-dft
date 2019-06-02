2af5d48214ac099130858db2383214be92a5c936
Andrey Sobolev <andrey.n.sobolev@gmail.com>
2019-06-02 23:53:07 +0500

    Added predefined basis families

--- aiida_crystal/data/basis_family.py
+++ aiida_crystal/data/basis_family.py
@@ -4,7 +4,7 @@
 A module describing the CRYSTAL basis family (Str on steroids)
 """
 
-from aiida.orm.data.str import Str
+from aiida.orm.data import Data
 
 BASIS_FAMILY_KWDS = [
     "STO-3G",
@@ -15,21 +15,15 @@ BASIS_FAMILY_KWDS = [
 ]
 
 
-class CrystalBasisFamilyData(Str):
-
-    def __init__(self, *args, **kwds):
-        super(CrystalBasisFamilyData, self).__init__(*args, **kwds)
+class CrystalBasisFamilyData(Data):
 
     @classmethod
     def _get(cls, name):
         # check if we can find the basis family
         from aiida.orm.querybuilder import QueryBuilder
         qb = QueryBuilder()
-        qb.append(cls)
-        for res in qb.all():
-            if res[0].get_attr('value') == name:
-                return res[0]
-        return None
+        qb.append(cls, filters={'attributes.name': {'==': name}})
+        return [res for [res] in qb.all()]
 
     @classmethod
     def get_or_create(cls, name, basis_sets=None):
@@ -41,19 +35,46 @@ class CrystalBasisFamilyData(Str):
         """
         found = cls._get(name)
         if found:
-            return found
+            return found[0]
         # creating basis family
         if name in BASIS_FAMILY_KWDS:
             if basis_sets is not None:
                 raise ValueError("{} is a predefined basis family in CRYSTAL; can't add basis sets to it".format(name))
             # return the instance
-            return cls(name)
+            return cls(name=name).store()
         return NotImplementedError
 
+    @property
+    def name(self):
+        return self.get_attr("name", default=None)
+
+    @name.setter
+    def name(self, value):
+        self._set_attr("name", value)
+
+    def set_name(self, name):
+        # check name for this instance
+        if self.name is not None:
+            raise ValueError("Name has already been set for this {} instance".format(self.__class__.__name__))
+        # check uniqueness
+        found = self._get(name)
+        if found and found[0].uuid != self.uuid:
+            raise ValueError("Found another {} instance in db with different uuid".format(self.__class__.__name__))
+        self.name = name
+
+    @property
+    def predefined(self):
+        return self.name in BASIS_FAMILY_KWDS
+
+    @property
     def content(self):
         """Content for adding to .d12
         """
-        if self.value in BASIS_FAMILY_KWDS:
-            return "BASISSET\n{}\n".format(self.value)
+        if self.predefined:
+            return "BASISSET\n{}\n".format(self.name)
         else:
             raise NotImplementedError
+
+    def store(self, with_transaction=True, use_cache=None):
+        return super(CrystalBasisFamilyData, self).store(with_transaction=with_transaction,
+                                                         use_cache=use_cache)
--- aiida_crystal/data/tests/test_basis_family.py
+++ aiida_crystal/data/tests/test_basis_family.py
@@ -1,14 +1,18 @@
 #  Copyright (c)  Andrey Sobolev, 2019. Distributed under MIT license, see LICENSE file.
 
-# noinspection pyUnresolvedReference
 from aiida_crystal.tests.fixtures import *
 
 
 def test_predefined_basis_family(aiida_profile):
-    from aiida_crystal.data.basis_family import CrystalBasisFamilyData
-    bf = CrystalBasisFamilyData.get_or_create('STO-3G')
-    bf.store()
-    bf2 = CrystalBasisFamilyData.get_or_create('STO-3G')
+    from aiida.orm import DataFactory
+    bf = DataFactory('crystal.basis_family').get_or_create('STO-3G')
+    bf2 = DataFactory('crystal.basis_family').get_or_create('STO-3G')
     assert bf2.uuid == bf.uuid
-    assert bf.content() == "BASISSET\nSTO-3G\n"
-    # assert bf2.content() == "BASISSET\nSTO-3G\n"
+    assert bf.content == "BASISSET\nSTO-3G\n"
+    assert bf2.content == "BASISSET\nSTO-3G\n"
+    assert bf2.predefined
+    with pytest.raises(ValueError):
+        DataFactory('crystal.basis_family')(name='STO-3G')
+    with pytest.raises(ValueError):
+        DataFactory('crystal.basis_family').get_or_create(name='STO-6G', basis_sets=["bs1", "bs2"])
+
