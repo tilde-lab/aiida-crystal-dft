ca2620092655dcee4aad26ce5db8c37b0c47f298
Andrey Sobolev <andrey.n.sobolev@gmail.com>
2020-02-12 19:53:48 +0500

    Amended example file to reflect changes

--- examples/test_MgO_MPDS.py
+++ examples/test_MgO_MPDS.py
@@ -54,7 +54,7 @@ inputs.mpds_query = DataFactory('dict')(dict={
 inputs.options = DataFactory('dict')(dict={
     'need_phonons': False,
     'need_electronic_properties': False,
-    'guess_oxistates': True,
+    'try_oxi_if_fails': True,
     'resources': {
         'num_machines': 1,
         'num_mpiprocs_per_machine': 1
