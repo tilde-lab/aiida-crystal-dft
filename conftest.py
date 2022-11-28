6e4b4506ab26f19aea6f076707ecd81518fd10ba
Sergey Korolev <korolev.srg@gmail.com>
2022-11-28 14:38:18 +0300

    fix: pytest dependencies

--- conftest.py
+++ conftest.py
@@ -1,5 +1,3 @@
 pytest_plugins = ['aiida.manage.tests.pytest_fixtures',
-                  'aiida_testing.mock_code',
-                  'aiida_testing.export_cache',
                   'aiida_crystal_dft.tests.fixtures']
 
