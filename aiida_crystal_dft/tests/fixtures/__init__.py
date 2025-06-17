4fb8ebc25eb5eba66b560c36ef0bfd612872b9b0
Evgeny Blokhin <eb@tilde.pro>
2025-06-17 23:18:38 +0200

    Use explicit imports

--- aiida_crystal_dft/tests/fixtures/__init__.py
+++ aiida_crystal_dft/tests/fixtures/__init__.py
@@ -1,5 +1,11 @@
 #  Copyright (c)  Andrey Sobolev, 2019. Distributed under MIT license, see LICENSE file.
 
-from .basis import *
-from .calculations import *
-from .code import *
+from .basis import test_basis_family, test_basis_family_predefined
+from .calculations import (
+    crystal_calc_inputs, calc_results, crystal_calc_node,
+    properties_calc_inputs, properties_calc_node, crystal_calc_parameters,
+    properties_calc_parameters, test_wavefunction, test_ase_structure,
+    test_mpds_structure, test_magnetic_structure, test_structure_data,
+    test_structure_issue_30
+)
+from .code import mock_crystal_code, mock_properties_code
